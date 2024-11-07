ea24a654a1f042b247b97ae1c9dfa3b31bb8fb92
davin-softdev <vinh.ly.softdev@gmail.com>
2024-11-07 11:19:39 +1030

    update responsive

--- src/components/NovelJustRead/NovelJustReadItem.tsx
+++ src/components/NovelJustRead/NovelJustReadItem.tsx
@@ -9,12 +9,16 @@ interface NovelJustReadItemProps {
 const NovelJustReadItem: React.FC<NovelJustReadItemProps> = ({ novel }) => {
   return (
     <div className="novel-just-read__item">
-      <span>{timeAgo(novel.readDate ?? new Date())}</span>
-      <h3>{novel.title}</h3>
-      <p>
-        Chapters read: {novel.read}/{novel.totalChapters} chapters
-      </p>
-      <button>x</button>
+      <div className="novel-just-read__item--header">
+        <span>{timeAgo(novel.readDate ?? new Date())}</span>
+        <h3>{novel.title}</h3>
+      </div>
+      <div className="novel-just-read__item--detail">
+        <p>
+          Chapters read: {novel.read}/{novel.totalChapters} chapters
+        </p>
+        <button>x</button>
+      </div>
     </div>
   )
 }
