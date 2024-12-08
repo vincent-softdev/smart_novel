4585cf20f38dcccf8134984e1f37cdb9320e40d3
davin-softdev <vinh.ly.softdev@gmail.com>
2024-12-08 22:25:43 +1030

    upload button with animation

--- src/components/nav-bar/RightNavBar.tsx
+++ src/components/nav-bar/RightNavBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import CloseIcon from '@mui/icons-material/Close'
+import UploadFileIcon from '@mui/icons-material/UploadFile'
 
 type RightNavBarProps = {
   handleAvartarClick: () => void
@@ -11,6 +12,10 @@ const RightNavBar = (props: RightNavBarProps) => {
       <div className="right-nav-bar__close-btn">
         <CloseIcon onClick={props.handleAvartarClick} />
       </div>
+      <button>
+        <UploadFileIcon />
+        Upload Novel
+      </button>
     </div>
   )
 }
